eec3eac555cc77813dd4d11ec9181bee860c66b0
Travis Wheeler <travis.wheeler130@yahoo.com>
2019-10-10 13:05:24 -0600

    initial commit

---
+++ DNDSpell/DNDSpell/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DNDSpell
+//
+//  Created by Travis Wheeler on 10/10/19.
+//  Copyright © 2019 Travis Wheeler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DNDSpell/DNDSpell/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DNDSpell
+//
+//  Created by Travis Wheeler on 10/10/19.
+//  Copyright © 2019 Travis Wheeler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
